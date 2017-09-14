75402e67b7660087cec8d1aa9025b79f59fe4155
Bob Bailey <bailey.robert.a@gmail.com>
2017-09-14 15:21:47 -0700

    add createActionItem endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
 
-from .views import getUsers, getCallSummary, getActionItems
+from .views import getUsers, getCallSummary, getActionItems, createActionItem
 
 urlpatterns = patterns('',
     url(r'^getUsers/?$', getUsers.as_view()),
     url(r'^getCallSummary/?$', getCallSummary.as_view()),
     url(r'^getActionItems/?$', getActionItems.as_view()),
     url(r'^getActionItems/is_complete=(?P<is_complete>\w+)/?$', getActionItems.as_view()),
+    url(r'^createActionItem/?$', createActionItem.as_view()),
 
-    url(r'^createActionItem/?$', getUsers.as_view()),
     url(r'^resolveeActionItem/?$', getUsers.as_view()),
     url(r'^getTickets/?$', getUsers.as_view()),
     url(r'^getTicketDetailByID/?$', getUsers.as_view()),
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
+from django import forms
 from django.http import HttpResponse
 from django.core import serializers
-
 from simple_rest import Resource
 
 from .models import Customer, Ticket, Note, ActionItem, Users 
@@ -9,7 +9,10 @@ from simple_rest.response import RESTfulResponse
 # TODO - @admin_required on all calls
 # TODO - login/logout calls
 # TODO - pagination
+
+
 class getUsers(Resource):
+    """ Get Users (return id, name, email) """
 
     @RESTfulResponse()
     def get(self, request, **kwargs):
@@ -17,6 +20,7 @@ class getUsers(Resource):
 
 
 class getCallSummary(Resource):
+    """ Get Call Summary (returns number of calls at each status) """
 
     @RESTfulResponse()
     def get(self, request, **kwargs):
@@ -27,6 +31,7 @@ class getCallSummary(Resource):
 
 
 class getActionItems(Resource):
+    """ Get Action Items for logged in user / status (returns a list of due date / description / is_complete for all action items associated with the user from the JWT token) - optionally can be filtered by is_complete=true or false """
 
     @RESTfulResponse()
     def get(self, request, is_complete=None, **kwargs):
@@ -39,3 +44,26 @@ class getActionItems(Resource):
         else:
             ai = ActionItem.objects.filter(rep_id=user_id)
         return ai
+
+
+class ActionItemForm(forms.ModelForm):
+    class Meta:
+        model = ActionItem
+        fields = ['rep_id', 'description', 'due_date']
+
+class createActionItem(Resource):
+    """ Create Action Item (takes rep_id for whom the action item will be created, description, due date, autopopulates created_by with the id of the user on the JWT token """
+
+    @RESTfulResponse()
+    def post(self, request):
+        form = ActionItemForm(request.POST)
+
+        if not form.is_valid():
+            return {"status":"Failure", "message":"Missing one or more form values"}
+
+        # TODO - add created_by user value
+        new_action_item = form.save(commit=False)
+        new_action_item.created_by = 1
+        new_action_item.save()
+        return {"status":"Success", "message":"Action item has been added"}
+
